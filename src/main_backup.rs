c16d0bdb618212b692205c658031c08ddc00dd1a
Glen Baker <iepathos@gmail.com>
2025-10-08 19:20:58 -0700

    cleanup

--- src/main_backup.rs
+++
@@ -1,3 +0,0 @@
-// Original main.rs backed up during spec 105 refactoring
-// This file contains the original 2,933-line main.rs implementation
-// It will be gradually extracted into the modular structure
